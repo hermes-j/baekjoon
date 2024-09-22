dd75d69e38ff5ab94cd5b7e1d241eccb71b13422
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2024-09-22 21:00:20 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+
+int main() {
+    fast_io;
+    int n, m, i, j;
+    cin >> n >> m;
+    int basket[100] = { 0 };
+
+    for (int a = 1; a <= n; a++) {
+        basket[a] = a;
+    }
+    for (int a = 0; a < m; a++) {
+        cin >> i >> j;
+        swap(basket[i], basket[j]);
+    }
+    for (int a = 1; a <= n; a++) {
+        cout << basket[a] << ' ';
+    }
+}
