3f90eb483570f75acf5a227889a97efe7583aacd
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2024-09-26 22:41:56 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+
+int main() {
+    fast_io;
+    string s;
+    int n;
+    cin >> s >> n;
+    cout << s[n - 1] << endl;
+}
