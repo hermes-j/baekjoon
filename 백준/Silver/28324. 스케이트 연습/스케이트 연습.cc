092fd6291f8c7a672b1293607a39874f6ebff644
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2025-11-09 14:26:57 +0900

    [Silver IV] Title: 스케이트 연습, Time: 64 ms, Memory: 5804 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28324. 스케이트 연습/스케이트 연습.cc
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.cc
@@ -1,15 +1,16 @@
 #include <bits/stdc++.h>
+typedef long long ll;
 using namespace std;
 
 int main() {
     cin.tie(NULL); ios_base::sync_with_stdio(false);
-    int n;
+    ll n;
 	cin >> n;
-	int arr[n];
+	ll arr[n];
 	for(int i = 1; i <= n; i++) {
 		cin >> arr[i];
 	}
-	int sum = 0, tmp = 0;
+	ll sum = 0, tmp = 0;
 	for(int i = n; i > 0; i--) {
 		tmp = min(tmp+1, arr[i]);
 		sum += tmp;
