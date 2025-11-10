ece8d99f737bf4396cff7e430b9be150bbdbbb1b
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2025-11-10 10:11:19 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    ll ur, tr, uo, to;
+	cin >> ur >> tr >> uo >> to;
+	cout << 56*ur + 24*tr + 14*uo + 6*to << "\n";
+    return 0;
+} 
