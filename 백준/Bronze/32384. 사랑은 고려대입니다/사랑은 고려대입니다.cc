b4612258227bbb44e1dcb09c88748b1d4daa660a
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2025-11-10 10:28:39 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    ll n;
+	cin >> n;
+	for(int i=0; i<n; i++) {
+		cout << "LoveisKoreaUniversity" << " ";
+	}
+    return 0;
+} 
