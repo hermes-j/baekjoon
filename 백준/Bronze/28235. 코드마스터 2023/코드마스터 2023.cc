a2abe3c450fc41c895e05074a111265d976139f0
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2025-11-10 10:13:48 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    string guho;
+	cin >> guho;
+	if (guho == "SONGDO") {
+		cout << "HIGHSCHOOL\n";
+	} else if (guho == "CODE") {
+		cout << "MASTER\n";
+	} else if (guho == "2023") {
+		cout << "0611\n";
+	} else if (guho == "ALGORITHM") {
+		cout << "CONTEST\n";
+	}
+    return 0;
+} 
