459344c9348da845fb2690ba29c85f42cac0662f
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2025-11-10 10:27:33 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    ll n, a;
+	cin >> n;
+	for(int i=0; i<n; i++) {
+		cin >> a;
+		cout << a << " " << a << "\n";
+	}
+} 
