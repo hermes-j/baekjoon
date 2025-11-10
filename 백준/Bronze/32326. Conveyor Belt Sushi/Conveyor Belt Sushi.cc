b48bc8fe987a5bec2efcbde3b5a63c058178622e
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2025-11-10 10:29:52 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    ll r, g, b;
+	cin >> r >> g >> b;
+	cout << (r*3 + g*4 + b*5) << "\n";
+    return 0;
+} 
