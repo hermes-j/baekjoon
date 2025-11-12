67284aba832983bcb7e56be499637094601cd52b
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2025-11-12 13:44:59 +0900

    [Bronze V] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    ll n; cin >> n;
+	for(ll i=0; i<n; i++) {
+		cout << "Hello World, Judge " << i+1 << "!\n";
+	}
+    return 0;
+} 
