3de4631e32fc86399a64b7611155d44cce4811aa
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2026-01-30 22:41:02 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    fast_io;
+    int n, k; cin >> n >> k;
+	int a, b;
+	while(n--) {
+		cin >> a >> b;
+	}
+	cout << "비와이" << "\n";
+    return 0;
+} 
