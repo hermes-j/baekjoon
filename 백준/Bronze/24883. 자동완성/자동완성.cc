c7611ac0a583c7035ee4d2cf7fd56e1accba6d90
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2026-01-31 14:53:34 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    fast_io;
+    char c; cin >> c;
+    if(c == 'n' || c == 'N') {
+        cout << "Naver D2" << "\n";
+    }
+    else cout << "Naver Whale" << "\n";
+    return 0;
+} 
