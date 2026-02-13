d0aa7a8b719845f7c582bdd117c40d31f1c99d71
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2026-02-13 18:51:58 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    fast_io;
+    int n;
+    cin >> n;
+    cout << n - 2024;
+    return 0;
+}
