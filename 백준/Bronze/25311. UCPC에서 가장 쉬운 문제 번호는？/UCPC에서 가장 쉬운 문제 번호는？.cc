319a9c1e50c5f80173669550635cfa293ecdc7f3
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2026-02-13 18:50:05 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    fast_io;
+    cout << "A";
+    return 0;
+}
