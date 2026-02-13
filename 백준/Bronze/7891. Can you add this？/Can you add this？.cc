f8068b35097cd2384ffe82b3dd958cd31c9b8a97
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2026-02-13 18:48:39 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    fast_io;
+    int t;
+	ll x, y;
+	cin >> t;
+	while(t--) {
+		cin >> x >> y;
+		cout << x + y << "\n";
+	}
+    return 0;
+}
