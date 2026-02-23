153330ab5f0727f003a1a9b7324e423840803586
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2026-02-23 15:47:58 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    fast_io;
+    cout << "     /~\\" << "\n" <<
+"    ( oo|" << "\n" <<
+"    _\\=/_" << "\n" <<
+"   /  _  \\" << "\n" <<
+"  //|/.\\|\\\\" << "\n" <<
+" ||  \\ /  ||" << "\n" <<
+"============" << "\n" <<
+"|          |" << "\n" <<
+"|          |" << "\n" <<
+"|          |" << "\n";
+    return 0;
+}
