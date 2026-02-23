bfc2a87ac22a76fa28d16302b0661a0b931f9bd0
HyungJun Lee <41940193+hermes-j@users.noreply.github.com>
2026-02-23 15:50:01 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    fast_io;
+    string id;
+	cin >> id;
+	cout << ":fan::fan::fan:\n:fan::" << id << "::fan:\n:fan::fan::fan:";
+    return 0;
+}
